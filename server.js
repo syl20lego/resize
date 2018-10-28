eb64568e8fa7db766d60edcdaf95a835e370bb2e
Sylvain Legault <slegault@aldogroup.com>
2018-10-27 22:05:34 -0400

    initial

---
+++ server.js
@@ -0,0 +1,53 @@
+const sharp = require('sharp')
+const glob = require('glob')
+
+const OPTIONS = {
+  // http://sharp.pixelplumbing.com/en/stable/
+  // jpegQuality: 80,
+  // jpegProgressive: false,
+  // jpegChromaSubsampling: '4:2:0',
+  // jpegTrellisQuantisation: false,
+  // jpegOvershootDeringing: false,
+  // jpegOptimiseScans: false,
+  // jpegOptimiseCoding: true,
+  // jpegQuantisationTable: 0,            
+}
+const SIZES = [600, 800, 1200, 1600]
+const FILES = 'img/**/*-original.jpg'
+
+const formats = file => SIZES.map(size => ({
+  size,
+  file: file.replace('original', size)
+}))
+
+const mapping = files =>
+  files.map(file => ({
+    source: file,
+    outputs: formats(file)
+  }))
+
+const listFiles = new Promise((resolve, reject) =>
+  glob(FILES, (err, files) =>
+    err ? reject(err) : resolve(files)
+  ))
+
+const resize = async () => {
+  try {
+    const files = await listFiles
+    mapping(files).map(async convert => {
+      const image = sharp(convert.source)
+      convert.outputs.map(async output => {
+        await image
+          .clone()
+          .jpeg(OPTIONS)
+          .resize(output.size)
+          .toFile(output.file)
+      })
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Execute resize
+resize()
